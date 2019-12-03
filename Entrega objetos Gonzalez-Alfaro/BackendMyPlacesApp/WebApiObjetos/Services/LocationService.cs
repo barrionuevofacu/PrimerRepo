6f669040bd27d371cc83c19bb4cf3da3a42a05be
facundob <facundob@unitech.com.ar>
2019-12-03 18:33:57 -0300

    Change apikey and api_id LocationService

--- Entrega objetos Gonzalez-Alfaro/BackendMyPlacesApp/WebApiObjetos/Services/LocationService.cs
+++ Entrega objetos Gonzalez-Alfaro/BackendMyPlacesApp/WebApiObjetos/Services/LocationService.cs
@@ -82,7 +82,7 @@ namespace WebApiObjetos.Services
 
         public async Task<ImageDTO> AddImage(ImageDTO image)
         {
-            IamAuthenticator authenticator = new IamAuthenticator(apikey: "qcuAtCisP-Au2RPtxkVM1pU4NsYzxu_iPTw9WlYSbOaq");
+            IamAuthenticator authenticator = new IamAuthenticator(apikey: "THrappn8HMg3bbxjUIFlyWDuJtN77Mbw-q1NWFuN6Ayc");
 
             VisualRecognitionService visualRecognition = new VisualRecognitionService("2018-03-19", authenticator);
             visualRecognition.SetServiceUrl("https://gateway.watsonplatform.net/visual-recognition/api");
@@ -110,7 +110,7 @@ namespace WebApiObjetos.Services
                         //url: "https://img.pixers.pics/pho_wat(s3:700/FO/48/14/15/73/700_FO48141573_3b497c03f0d6755bb5657b67149c578d.jpg,700,507,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,480,457,jpg)/vinilos-para-armario-alaskan-malamute-en-la-nieve.jpg.jpg",
                         imagesFilename: "MyTest.jpeg",
                         imagesFile: ms,
-                        classifierIds: new List<string>() { "Clasificaperros_187924846" },
+                        classifierIds: new List<string>() { "razaperros_1652086664" },
                         //threshold: 0.6f,
                         owners: new List<string>() { "me" }
                     );
